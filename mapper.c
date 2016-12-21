59dfe55633a6d8d3eed90266f0d4224379a74a66
Jolieczl <lemonweed@163.com>
2016-12-21 15:31:39 +0800

    hadoop-mapreduce
    hadoop-mapreduce

---
+++ mapper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <string.h>  
+  
+#define BUF_SIZE    2048  
+#define DELIM       '\n'  
+  
+int main(int argc, char * argv[])  
+{  
+    char buffer[BUF_SIZE];  
+    while(fgets(buffer,BUF_SIZE-1,stdin))  
+    {  
+        int len = strlen(buffer);  
+        if(buffer[len-1] == DELIM) // 将换行符去掉  
+            buffer[len-1] = 0;  
+  
+        char *query = NULL;  
+        query = strtok(buffer, " ");  
+        while(query)  
+        {  
+            printf("%s\t1\n",query);  
+            query = strtok(NULL," ");  
+        }  
+    }  
+    return 0;  
+}
